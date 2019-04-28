480aa9c6c65a152a9b0b94fa971b11e0317d30ca
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-04-28 14:31:45 +0530

    Fullfilled a promise exercise completed

---
+++ fulfill-a-promise.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(() => {
+        fulfill('FULFILLED!');
+    }, 300);
+});
+
+promise.then((data) => {
+    console.log(data);
+});
