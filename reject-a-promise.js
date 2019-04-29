f61b3ac3923a9c257e9bc481de526546c9bef3b1
Jatin Panjwani <jatinpanjwani12@gmail.com>
2019-04-29 10:36:52 +0530

    Rejecting promise exercise completed

--- reject-a-promise.js
+++ reject-a-promise.js
@@ -1,1 +1,13 @@
-''
+var promise = new Promise(function (fulfill, reject) {
+    // Your solution here
+    setTimeout(() => {
+        reject(new Error('REJECTED!'));
+    }, 300);
+});
+
+function onReject(error) {
+    // Your solution here
+    console.log(error.message);
+}
+
+promise.then(null, onReject);
